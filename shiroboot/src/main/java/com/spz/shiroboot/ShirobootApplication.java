560b7f31540ae75986314c3216284ec2a51d8bc1
sunpengzhen <sunpengzhen@hxdi.com>
2023-02-07 17:18:34 +0800

    shiroBoot初始化

---
+++ shiroboot/src/main/java/com/spz/shiroboot/ShirobootApplication.java
@@ -0,0 +1,13 @@
+package com.spz.shiroboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShirobootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShirobootApplication.class, args);
+    }
+
+}
---
+++ shiroboot/src/test/java/com/spz/shiroboot/ShirobootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spz.shiroboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShirobootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
