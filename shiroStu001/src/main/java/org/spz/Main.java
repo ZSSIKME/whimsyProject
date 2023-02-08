00f23f3e18ae29d442ee98ce3e6527215b4d54ac
sunpengzhen <sunpengzhen@hxdi.com>
2023-02-08 16:36:49 +0800

    加密解密

--- shiroStu001/src/main/java/org/spz/Main.java
+++ shiroStu001/src/main/java/org/spz/Main.java
@@ -2,11 +2,18 @@ package org.spz;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.codec.Base64;
 import org.apache.shiro.codec.Hex;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.junit.jupiter.api.Test;
+
+import javax.crypto.Cipher;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.X509EncodedKeySpec;
 
 public class Main {
     public static void main(String[] args) {
@@ -24,5 +31,19 @@ public class Main {
         UsernamePasswordToken spz = new UsernamePasswordToken("spz", "123");
         subject.login(spz);
         System.out.println("==="+subject.isAuthenticated());
+    }
+
+    @Test
+    public void demo() throws Exception {
+//        byte[] decoded = Base64.decodeBase64("");
+//        RSAPublicKey pubKey = (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(decoded));
+//        Cipher cipher = Cipher.getInstance("RSA");
+//        cipher.init(1, pubKey);
+//        String supername = Base64.encodeBase64String(cipher.doFinal("super".getBytes("UTF-8")));
+//        String password = Base64.encodeBase64String(cipher.doFinal("1".getBytes("UTF-8")));
+//        System.out.println(supername+"===="+password);
+
+        String s = Base64.encodeToString("super".getBytes());
+        System.out.println(s);
     }
 }
